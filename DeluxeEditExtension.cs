e73810b4cb577c4d2c18b801465aac6c88b8d189
Pierre Axelsson <pierre.axelsson@outlook.com>
2025-10-23 13:51:30 +0200

    Added from new project

---
+++ DeluxeEditExtension.cs
@@ -0,0 +1,56 @@
+﻿using SharpShell.SharpContextMenu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace ShellExtensions
+{
+    /// <summary>
+    /// The Count Lines Context Menu Extension
+    /// </summary>
+    public class DeluxeEditExtension : SharpContextMenu
+    {
+        const string CommandToRun = "PyDeluxeEdit";
+
+        protected override bool CanShowMenu()
+        {
+            //  We will always show the menu
+            return true;
+        }
+
+        protected override ContextMenuStrip CreateMenu()
+        {
+            //  Create the menu strip
+            var menu = new ContextMenuStrip();
+
+            //  Create a 'count lines' item
+            var deluxeItem = new ToolStripMenuItem();
+            deluxeItem.Text = "DeluxeEdit...";
+            deluxeItem.Image = Properties.Resources.deluxe;
+
+            //  When we click, we'll call the 'CountLines' function
+            deluxeItem.Click += (sender, args) => startProgram();
+
+            //  Add the item to the context menu
+            menu.Items.Add(deluxeItem);
+
+            //  Return the menu
+            return menu;
+        }
+        private void startProgram()
+        {
+            var selectedPath = SelectedItemPaths.FirstOrDefault();
+            if (selectedPath != null)
+            {
+            }
+            //  Go through each fileS
+            //  Count the lines
+        }
+
+      
+     
+    }
+
+}
---
+++ DeluxeHexViewExtension.cs
@@ -0,0 +1,54 @@
+﻿using SharpShell.SharpContextMenu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace ShellExtensions
+{
+    /// <summary>
+    /// The Count Lines Context Menu Extension
+    /// </summary>
+    public class DeluxeHexViewExtension : SharpContextMenu
+    {
+        const string CommandToRun = "PyDeluxeEdit --hex";
+        protected override bool CanShowMenu()
+        {
+            //  We will always show the menu
+            return true;
+        }
+
+        protected override ContextMenuStrip CreateMenu()
+        {
+            //  Create the menu strip
+            var menu = new ContextMenuStrip();
+            
+            var deluxeItem = new ToolStripMenuItem();
+            deluxeItem.Text = "DeluxeHexView...";
+            deluxeItem.Image = Properties.Resources.deluxe;
+
+
+            //  When we click, we'll call the 'CountLines' function
+            deluxeItem.Click += (sender, args) => starProlgram();
+
+            //  Add the item to the context menu
+            menu.Items.Add(deluxeItem);
+
+            //  Return the menu
+            return menu;
+        }
+
+        private void starProlgram()
+        {
+            var selectedPath = SelectedItemPaths.FirstOrDefault();
+            if (selectedPath != null)
+            { 
+            }
+            //  Go through each fileS
+                //  Count the lines
+        }
+
+    }
+
+}
